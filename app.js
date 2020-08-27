fdd0953f73882a6e512d815a06b54c3ba5035f55
ebeagusamuel <samuelebeagu@gmail.com>
2020-08-27 15:23:12 +0100

    create function to add book to library

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let library = []
+
 function Book(title, author, numOfPages, read = false){
   this.title = title;
   this.author = author;
@@ -5,3 +7,16 @@ function Book(title, author, numOfPages, read = false){
   this.read = read;
   this.info =  () => `${this.title} by ${this.author}, ${this.numOfPages} pages, read: ${this.read}.`
 }
+
+
+function addBookToLibrary(){
+  let title = document.getElementById('title').value
+  let author = document.getElementById('author').value
+  let pages = document.getElementById('pages').value
+  let read = document.getElementById('read').value;
+
+  let book = new Book(title, author, pages, read);
+  library.push(book)
+  
+}
+
