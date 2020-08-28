52184cdac59f1f06f4e0a0ea165f40d3a482aa90
ebeagusamuel <samuelebeagu@gmail.com>
2020-08-28 09:41:59 +0100

    render book cards dynamically

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let library = []
+const form = document.getElementById('form')
 
 function Book(title, author, numOfPages, read = false){
   this.title = title;
@@ -9,7 +10,7 @@ function Book(title, author, numOfPages, read = false){
 }
 
 
-function addBookToLibrary(){
+function addBookToLibrary(e){
   let title = document.getElementById('title').value
   let author = document.getElementById('author').value
   let pages = document.getElementById('pages').value
@@ -17,5 +18,66 @@ function addBookToLibrary(){
 
   let book = new Book(title, author, pages, read);
   library.push(book) 
+}
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let title = e.target.elements.title.value;
+  let author = e.target.elements.author.value;
+  let pages = e.target.elements.pages.value;
+  let read = e.target.elements.read.value;
+
+  addBookToLibrary(title, author, pages, read);
+
+  render()
+})
+
+function render(){
+  var div = document.createElement('div')
+  div.classList.add('card')
+  div.classList.add('m-3')
+  div.style.width = '18rem';
+
+  var ul = document.createElement('ul')
+  ul.classList.add('list-group');
+  ul.classList.add('list-group-flush');
+
+  var firstList = document.createElement('li');
+  firstList.classList.add('list-group-item')
+  firstList.textContent = `Title: ${library[0].title}`;
+
+  var secondList = document.createElement('li');
+  secondList.classList.add('list-group-item')
+  secondList.textContent = `Author: ${library[0].author}`
+
+  var thirdList = document.createElement('li');
+  thirdList.classList.add('list-group-item')
+  thirdList.textContent = `Pages: ${library[0].numOfPages}`
+
+  var fourthList = document.createElement('li');
+  fourthList.classList.add('list-group-item')
+  fourthList.textContent = `Pages: ${library[0].read}`
+
+  var fifthList = document.createElement('li');
+  fifthList.classList.add('list-group-item')
+
+  var button = document.createElement('button');
+  button.classList.add('btn');
+  button.classList.add('btn-primary');
+  button.textContent = 'Delete';
+
+  fifthList.appendChild(button)
+
+  ul.appendChild(firstList);
+  ul.appendChild(secondList);
+  ul.appendChild(thirdList);
+  ul.appendChild(fourthList);
+  ul.appendChild(fifthList);
+
+  div.appendChild(ul);
+
+  var container = document.getElementById('card-container');
+  container.appendChild(div);
 }
 
