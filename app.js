f1e132e7f7ecf7a7ee416b44b8c6dfa517569a5f
jstloyal <jstloyalty@gmail.com>
2020-08-31 14:57:59 +0100

    Add Read toggle and fix lint error

--- app.js
+++ app.js
@@ -1,78 +1,68 @@
-let library = []
+const library = [];
 let index = -1;
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
-function Book(title, author, numOfPages, read = false){
+function Book(title, author, numOfPages, read = false) {
   this.title = title;
   this.author = author;
   this.numOfPages = numOfPages;
   this.read = read;
-  this.info =  () => `${this.title} by ${this.author}, ${this.numOfPages} pages, read: ${this.read}.`
+  this.info = () => `${this.title} by ${this.author}, ${this.numOfPages} pages, read: ${this.read}.`;
 }
 
-function addBookToLibrary(e){
-  let title = document.getElementById('title').value
-  let author = document.getElementById('author').value
-  let pages = document.getElementById('pages').value
-  let read = document.getElementById('read').value;
-
-  let book = new Book(title, author, pages, read);
-  library.push(book) 
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  library.push(book);
 }
 
+function clearInputField() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
+}
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  index += 1;
-
-  let title = e.target.elements.title.value;
-  let author = e.target.elements.author.value;
-  let pages = e.target.elements.pages.value;
-  let read = e.target.elements.read.value;
-
-  addBookToLibrary(title, author, pages, read);
-
-  document.getElementById('close-modal').click();
-
-  render();
-})
-
-function render(){
-  var div = document.createElement('div')
-  div.classList.add('card')
-  div.classList.add('m-3')
+function render() {
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.classList.add('m-3');
   div.style.width = '18rem';
 
-  var ul = document.createElement('ul')
+  const ul = document.createElement('ul');
   ul.classList.add('list-group');
   ul.classList.add('list-group-flush');
 
-  var firstList = document.createElement('li');
-  firstList.classList.add('list-group-item')
+  const firstList = document.createElement('li');
+  firstList.classList.add('list-group-item');
   firstList.textContent = `Title: ${library[index].title}`;
 
-  var secondList = document.createElement('li');
-  secondList.classList.add('list-group-item')
-  secondList.textContent = `Author: ${library[index].author}`
+  const secondList = document.createElement('li');
+  secondList.classList.add('list-group-item');
+  secondList.textContent = `Author: ${library[index].author}`;
 
-  var thirdList = document.createElement('li');
+  const thirdList = document.createElement('li');
   thirdList.classList.add('list-group-item');
   thirdList.textContent = `Pages: ${library[index].numOfPages}`;
 
-  var fourthList = document.createElement('li');
-  fourthList.classList.add('list-group-item')
-  fourthList.textContent = `Read: ${library[index].read}`
+  const fourthList = document.createElement('li');
+  fourthList.classList.add('list-group-item');
+  fourthList.textContent = `Read: ${library[index].read}`;
 
-  var fifthList = document.createElement('li');
-  fifthList.classList.add('list-group-item')
+  const fifthList = document.createElement('li');
+  fifthList.classList.add('list-group-item');
 
-  var button = document.createElement('button');
+  const button = document.createElement('button');
   button.classList.add('btn');
   button.classList.add('btn-primary');
   button.textContent = 'Delete';
 
-  fifthList.appendChild(button)
+  const readButton = document.createElement('button');
+  readButton.classList.add('btn');
+  readButton.classList.add('btn-primary');
+  readButton.classList.add('ml-2');
+  readButton.textContent = 'Toggle Read';
+
+  fifthList.appendChild(button);
+  fifthList.appendChild(readButton);
 
   ul.appendChild(firstList);
   ul.appendChild(secondList);
@@ -82,21 +72,43 @@ function render(){
 
   div.appendChild(ul);
 
-  var container = document.getElementById('card-container');
+  const container = document.getElementById('card-container');
   container.appendChild(div);
 
-  clearInputField()
-
+  clearInputField();
 }
 
-function clearInputField(){
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('pages').value = '';
-}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-document.querySelector('#card-container').addEventListener('click', e => {
-  if(e.target.innerHTML == 'Delete'){
-    e.target.parentElement.parentElement.parentElement.remove()
+  index += 1;
+
+  const title = e.target.elements.title.value;
+  const author = e.target.elements.author.value;
+  const pages = e.target.elements.pages.value;
+  const read = e.target.elements.read.value;
+
+  addBookToLibrary(title, author, pages, read);
+
+  document.getElementById('close-modal').click();
+
+  render();
+});
+
+document.querySelector('#card-container').addEventListener('click', (e) => {
+  if (e.target.innerHTML === 'Delete') {
+    e.target.parentElement.parentElement.parentElement.remove();
   }
-})
+});
+
+document.querySelector('#card-container').addEventListener('click', (e) => {
+  if (e.target.innerHTML === 'Toggle Read') {
+    const parentNode = e.target.parentElement.parentElement;
+    const text = parentNode.childNodes[3].innerHTML;
+    if (text === 'Read: No') {
+      parentNode.childNodes[3].innerHTML = 'Read: Yes';
+    } else {
+      parentNode.childNodes[3].innerHTML = 'Read: No';
+    }
+  }
+});
